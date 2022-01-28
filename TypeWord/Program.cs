549004c3c2584beb5e913010d8aa164d35026686
VINKOS <kravter@gmail.com>
2022-01-28 15:19:51 +0300

    add WordsDB

--- TypeWord/Program.cs
+++ TypeWord/Program.cs
@@ -17,16 +17,17 @@ namespace TypeWord
     class TypeWord
     {
         const char SymbolStartPartWord = '?';
-        const string PathPriority = "../LibraryMorphemes/Priority.txt";
-        const string PathLibraryMorphemes = "../LibraryMorphemes/";
+        const string PathPriority = @"..\MyNET\Services\LibraryMorphemes\Priority.txt";
+        const string PathLibraryMorphemes = @"..\MyNET\Services\LibraryMorphemes";
+        const string PathWordsDataBase = @"..\MyNET\Services\LibraryMorphemes\WordsDataBase";
         public class Word
         {
-            public string Symbols {get;}
-            public string Type {get;}
+            public string Symbols { get; }
+            public string Type { get; }
 
             public Word(string _Symbols)
             {
-                Symbols = _Symbols;
+                Symbols = _Symbols.ToLower();
 
                 Type = IsType(Symbols);
             }
@@ -34,8 +35,10 @@ namespace TypeWord
             private string IsType(string word)
             {
                 List<string[]> Morphemes = new();
+                List<string[]> WordsDB = new();
 
                 string[] MorphemesNamesFiles;
+                string[] WordsDBNamesFiles;
                 string[] MorphemesNamesFilesPriority;
 
                 if (Directory.Exists(PathLibraryMorphemes))
@@ -55,28 +58,56 @@ namespace TypeWord
                     for (int i = 0; i < MorphemesNamesFiles.Length; i++)
                         Morphemes.Add(File.ReadAllLines(MorphemesNamesFiles[i]));
 
+                    if (Directory.Exists(PathWordsDataBase))
+                    {
+                        WordsDBNamesFiles = Directory.GetFiles(PathWordsDataBase);
+
+                        for (int i = 0; i < WordsDBNamesFiles.Length; i++)
+                            WordsDB.Add(File.ReadAllLines(WordsDBNamesFiles[i]));
+
+                        for (int i = 0; i < WordsDB.Count; i++)
+                        {
+                            if (SearchMorpheme(word, WordsDB[i], true) != null)
+                            {
+                                Regex NameFile = new Regex(@"\\\w{1,}\.");
+
+                                return NameFile.Match(WordsDBNamesFiles[i]).Value.Trim('\\', '.');
+                            }
+                        }
+                    }
+
                     for (int i = 0; i < Morphemes.Count; i++)
                     {
                         if (SearchMorpheme(word, Morphemes[i]) != null)
                         {
-                            Regex NameFile = new Regex(@"\/\w{1,}\.");
+                            Regex NameFile = new Regex(@"\\\w{1,}\.");
 
-                            return NameFile.Match(MorphemesNamesFiles[i]).Value.Trim('/','.');
+                            return NameFile.Match(MorphemesNamesFiles[i]).Value.Trim('\\', '.');
                         }
                     }
                 }
 
                 return "Noun";
             }
-            private string SearchMorpheme(string word, string[] Morphemes)
+            private string SearchMorpheme(string word, string[] Morphemes, bool IsFullW = false)
             {
-                for (int i = 0; i < Morphemes.Length; i++)
+                int idx = 0;
+
+                if (Morphemes[0] == "IsFullW")
+                {
+                    IsFullW = true;
+                    idx = 1;
+                }
+
+                for (int i = idx; i < Morphemes.Length; i++)
                 {
                     if (!Morphemes[i].Contains(SymbolStartPartWord))
                     {
-                        if (word.Length - Morphemes[i].Length >= 0)
-                            if (word.Substring(word.Length - Morphemes[i].Length) == Morphemes[i])
-                                return Morphemes[i];
+                        if (IsFullW == false) { if (word.Length - Morphemes[i].Length < 1) continue; }
+                        else if (word.Length - Morphemes[i].Length != 0) continue;
+
+                        if (word.Substring(word.Length - Morphemes[i].Length) == Morphemes[i])
+                            return Morphemes[i];
                     }
                     else
                     {
@@ -120,7 +151,7 @@ namespace TypeWord
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            TypeWord.Word WordTXT = new("Прыгающий");
+            TypeWord.Word WordTXT = new("коля");
         }
     }
 }
